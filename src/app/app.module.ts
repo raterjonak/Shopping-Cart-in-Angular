cf637868949af1b8c9d354d60c5c68cba12d5fac
raterjonak <monir.mahmud8@gmail.com>
2022-08-19 16:04:09 +0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
